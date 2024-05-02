bd16e94338a5cfd70e5ebc24cc46e4fd6af63b43
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-02 14:29:44 +0700

    Update datagen.tf

--- datagen.tf
+++ datagen.tf
@@ -7,9 +7,6 @@ terraform {
   }
 }
 
-variable "region" {
-  default = "ap-southeast-1"
-}
 provider "aws" {
   region = var.region
 }
@@ -17,20 +14,20 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "staging-area" {
-  bucket = "${data.aws_caller_identity.current.account_id}-staging-area"
+  bucket        = "${data.aws_caller_identity.current.account_id}-staging-area"
   force_destroy = true
 }
 
 resource "aws_instance" "data-generator" {
-  instance_type = "t2.micro"
-  ami           = "ami-0be48b687295f8bd6"
-  user_data     = <<EOF
+  instance_type        = "t2.micro"
+  ami                  = "ami-0be48b687295f8bd6"
+  user_data            = <<EOF
 #!/bin/bash
 export S3_BUCKET=${aws_s3_bucket.staging-area.id}
 export AWS_REGION=${var.region}
 ${file("./datagen/setup.sh")}
 EOF
-  iam_instance_profile = "EC2-role"
+  iam_instance_profile = var.s3-role-name
   tags = {
     Name = "data-generator"
   }
