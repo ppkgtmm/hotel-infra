fa622da67696106884f960ef1a4d84d68fb8d639
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-03 01:04:40 +0700

    Update setup.sh

--- dataseed/setup.sh
+++ dataseed/setup.sh
@@ -1,5 +1,5 @@
 apt update
-apt install -y git python3 python3-venv wget
+apt install -y git python3 python3-venv wget postgresql15
 git clone https://github.com/ppkgtmm/hotel-seed.git dataseed
 cd dataseed
 wget -O .env https://raw.githubusercontent.com/ppkgtmm/hotel-infra/main/.env
