30377fae4bbdf874ce5aa05fc7acf7de2b9b5f13
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-02 21:30:01 +0700

    Update setup.sh

--- datagen/setup.sh
+++ datagen/setup.sh
@@ -1,5 +1,6 @@
 apt update
-apt install -y git python3 python3-venv
+apt install -y git python3 python3-venv wget
 git clone https://github.com/ppkgtmm/hotel-datagen.git datagen
 cd datagen
+wget -O .env https://raw.githubusercontent.com/ppkgtmm/hotel-infra/main/.env
 ./run.sh
