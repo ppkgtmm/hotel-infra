e67f8f7b55727e3edc0c226c2936bb90fe5a47f3
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-03 00:12:36 +0700

    Create setup.sh

---
+++ dataseed/setup.sh
@@ -0,0 +1,6 @@
+apt update
+apt install -y git python3 python3-venv wget
+git clone https://github.com/ppkgtmm/hotel-seed.git dataseed
+cd dataseed
+wget -O .env https://raw.githubusercontent.com/ppkgtmm/hotel-infra/main/.env
+./run.sh
