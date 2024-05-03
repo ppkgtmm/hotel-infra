ad90ae45b625ff77d0b15c9eaa9eaa2030900368
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-04 01:06:04 +0700

    Update setup.sh

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -3,5 +3,6 @@ apt install -y wget default-jdk
 wget -O kafka.tgz https://dlcdn.apache.org/kafka/3.7.0/kafka_2.13-3.7.0.tgz
 tar xzf kafka.tgz
 cd kafka_2.13-3.7.0
-bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config/kraft/server.properties
-bin/kafka-server-start.sh config/kraft/server.properties
+wget -O config.properties https://github.com/ppkgtmm/hotel-infra/raw/main/kafka/kafka.properties
+bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config.properties
+bin/kafka-server-start.sh config.properties
