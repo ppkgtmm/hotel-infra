b64a8f87f503e42f31afe7338da33ff1912b2958
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-03 22:54:13 +0700

    Create kafka.tf

---
+++ kafka.tf
@@ -0,0 +1,35 @@
+resource "random_uuid" "rand" {}
+
+resource "google_compute_address" "kafka" {
+  name = "ipv4-address"
+}
+
+resource "google_compute_instance" "kafka" {
+  machine_type   = "e2-micro"
+  name           = "kafka"
+  enable_display = false
+  boot_disk {
+    initialize_params {
+      image = var.gcp_disk_image
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.kafka.address
+    }
+  }
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+  }
+  metadata_startup_script = <<EOF
+#!/bin/bash
+export KAFKA_CLUSTER_ID=${random_uuid.rand.id}
+${file("./kafka/setup.sh")}
+EOF
+}
