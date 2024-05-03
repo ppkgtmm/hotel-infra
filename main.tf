78ba9b507ab95dec8f5641b9222158df58966fc6
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-03 16:53:44 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,47 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.47.0"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "5.27.0"
+    }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = var.aws_region
+}
+
+provider "google" {
+  project = var.gcp-project-id
+  region  = var.gcp_region
 }
 
 data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# data "google_compute_subnetwork" "default" {
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
+#   security_group_id = data.aws_security_group.default.id
+#   ip_protocol       = "tcp"
+#   cidr_ipv4         = data.google_compute_network.default.gateway_ipv4
+#   from_port         = 5432
+#   to_port           = 5432
+# }
+
+output "aws_vpc" {
+  value = data.aws_vpc.default.id
+}
+
+output "gcp_vpc" {
+  value = data.google_compute_subnetwork.default.ip_cidr_range
+}
