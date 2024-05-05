e31e137a1a2ea5454b4e262471e444bc10988b17
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 14:22:45 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,20 +27,20 @@ provider "google" {
 
 provider "random" {}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# data "aws_vpc" "default" {
-#   default = true
-# }
+data "aws_vpc" "default" {
+  default = true
+}
 
-# data "aws_security_group" "default" {
-#   vpc_id = data.aws_vpc.default.id
-# }
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
 
-# resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
-#   security_group_id = data.aws_security_group.default.id
-#   ip_protocol       = "tcp"
-#   cidr_ipv4         = data.google_compute_network.default.gateway_ipv4
-#   from_port         = 5432
-#   to_port           = 5432
-# }
+resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
+  security_group_id = data.aws_security_group.default.id
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "${google_compute_instance.kafka-connect.network_interface[0].access_config[0].nat_ip}/32"
+  from_port         = 5432
+  to_port           = 5432
+}
