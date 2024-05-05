f9894f80fc20933168628d2f3b55a71855bc5651
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 16:58:43 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "aws_zone" {
   default = "ap-southeast-1c"
 }
 
-variable "data-generator-role" {
-  default = "s3-access"
-}
-
 variable "data-seeder-role" {
   default = "rds-s3-access"
 }
