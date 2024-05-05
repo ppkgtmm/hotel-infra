b868fcc7b97b9c42eb5442a7a62f187a0e021b9b
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 17:09:24 +0700

    Create main.tf

---
+++ kafka-connect/main.tf
@@ -0,0 +1,40 @@
+resource "google_compute_instance" "kafka-connect" {
+  machine_type   = "e2-micro"
+  name           = "kafka-connect"
+  enable_display = false
+  tags           = ["kafka-connect"]
+  boot_disk {
+    initialize_params {
+      image = var.gcp_disk_image
+      size  = 10
+      type  = "pd-standard"
+    }
+  }
+  network_interface {
+    network = var.gcp_network
+    access_config {
+    }
+  }
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+  }
+  metadata_startup_script = <<EOF
+#!/bin/bash
+export KAFKA_SERVER=${var.kafka_server}
+${file("./setup.sh")}
+EOF
+}
+
+resource "google_compute_firewall" "name" {
+  network     = var.gcp_network
+  name        = "allow-kafka-internal"
+  target_tags = ["kafka"]
+  source_tags = ["kafka-connect"]
+  allow {
+    protocol = "tcp"
+    ports    = ["9092"]
+  }
+}
