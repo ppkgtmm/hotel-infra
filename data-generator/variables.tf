b753d3061354f6d8abec647faa359fca951c3dfb
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 16:58:35 +0700

    Create variables.tf

---
+++ data-generator/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_zone" {
+  type = string
+}
+
+variable "data-generator-role" {
+  default = "s3-access"
+}
+
+variable "account-id" {
+  type = string
+}
