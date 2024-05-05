1907570befe8ef1b5602f7c5786f2cdf62b1e0a5
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 17:02:04 +0700

    Create variables.tf

---
+++ data-seeder/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_zone" {
+  type = string
+}
+
+variable "data-seeder-role" {
+  default = "rds-s3-access"
+}
+
+variable "source-db-name" {
+  type = string
+}
+
+variable "source-db-username" {
+  type      = string
+  sensitive = true
+}
+
+variable "source-db-password" {
+  type      = string
+  sensitive = true
+}
