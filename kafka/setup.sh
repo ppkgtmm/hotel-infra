e865f48ad34f8b8564614122739ed8fe38d5ba27
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 12:32:05 +0700

    Update setup.sh

--- kafka/setup.sh
+++ kafka/setup.sh
@@ -4,6 +4,7 @@ wget -O kafka.tgz https://dlcdn.apache.org/kafka/3.7.0/kafka_2.13-3.7.0.tgz
 tar xzf kafka.tgz
 cd kafka_2.13-3.7.0
 wget -O config.properties https://github.com/ppkgtmm/hotel-infra/raw/main/kafka/kafka.properties
+export KAFKA_OPTS="-Djava.net.preferIPv4Stack=True"
 # echo "advertised.listeners=PLAINTEXT://$KAFKA_SERVER:9092" >> config.properties
 bin/kafka-storage.sh format -t $KAFKA_CLUSTER_ID -c config.properties
 bin/kafka-server-start.sh config.properties
