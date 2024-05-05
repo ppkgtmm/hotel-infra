57140b524b3df647ff7c56d7b23548e298ebea42
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-05 17:04:33 +0700

    Update kafka-connect.tf

--- kafka-connect.tf
+++ kafka-connect.tf
@@ -1,35 +1,3 @@
-resource "random_uuid" "rand" {}
-
-resource "google_compute_instance" "kafka" {
-  machine_type   = "e2-micro"
-  name           = "kafka"
-  enable_display = false
-  boot_disk {
-    initialize_params {
-      image = var.gcp_disk_image
-      size  = 10
-      type  = "pd-standard"
-    }
-  }
-  tags = ["kafka"]
-  network_interface {
-    network = var.gcp_network
-    access_config {
-    }
-  }
-  scheduling {
-    automatic_restart   = false
-    on_host_maintenance = "TERMINATE"
-    preemptible         = true
-    provisioning_model  = "SPOT"
-  }
-  metadata_startup_script = <<EOF
-#!/bin/bash
-export KAFKA_CLUSTER_ID=${random_uuid.rand.id}
-${file("./kafka/setup.sh")}
-EOF
-}
-
 resource "google_compute_instance" "kafka-connect" {
   machine_type   = "e2-micro"
   name           = "kafka-connect"
