64504a8c5c9f5759b76871a4c93ca1fc76c6eb02
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 15:01:10 +0700

    Update variables.tf

--- connector/variables.tf
+++ connector/variables.tf
@@ -2,12 +2,8 @@ variable "bucket-id" {
   type = string
 }
 
-variable "object-key" {
-  default = "debezium-connector-postgres-2.6.1.Final-plugin.tar.gz"
-}
-
-variable "object-source" {
-  default = "https://repo1.maven.org/maven2/io/debezium/debezium-connector-postgres/2.6.1.Final/debezium-connector-postgres-2.6.1.Final-plugin.tar.gz"
+variable "plugin" {
+  default = "debezium-connector-postgres-2.6.1"
 }
 
 variable "aws-region" {
