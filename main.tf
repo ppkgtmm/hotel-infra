67433145044f9687b4ca92b0e297a3f4cead2b69
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 19:39:42 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,21 @@ provider "random" {}
 
 data "aws_caller_identity" "current" {}
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 module "data-generator" {
   source            = "./data-generator"
   aws-zone          = var.aws-zone
