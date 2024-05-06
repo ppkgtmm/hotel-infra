81ef4a077798ba72bf0252297ef62c073cdd090a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 19:43:46 +0700

    Update main.tf

--- kafka/main.tf
+++ kafka/main.tf
@@ -1,31 +1,16 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
 resource "aws_msk_cluster" "hotel-kafka" {
   cluster_name           = "hotel-kafka"
   kafka_version          = "3.5.1"
   number_of_broker_nodes = 2
 
   broker_node_group_info {
-    instance_type  = "kafka.m7g.large"
-    client_subnets = [data.aws_subnets.subnets.ids[0], data.aws_subnets.subnets.ids[1]]
+    instance_type  = "kafka.t3.small"
+    client_subnets = var.subnets
     storage_info {
       ebs_storage_info {
         volume_size = 5
       }
     }
-    security_groups = [data.aws_security_group.default.id]
+    security_groups = [var.security-group-id]
   }
 }
