8b1c644114c8b3e36485192daca215a6b1171c87
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 15:01:16 +0700

    Update main.tf

--- connector/main.tf
+++ connector/main.tf
@@ -1,6 +1,13 @@
 resource "aws_instance" "connector" {
   instance_type        = var.aws-instance-type
   ami                  = var.aws-ami
+  user_data            = <<EOF
+#!/bin/bash
+export S3_BUCKET=${var.bucket-id}
+export AWS_REGION=${var.aws-region}
+export PLUGIN=${var.plugin}
+${file("./connector/setup.sh")}
+EOF
   iam_instance_profile = var.connector-role
   tags = {
     Name = "connector-setup"
