41624247c0cc7ff58cc446afce50918cdd210539
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 20:22:57 +0700

    Update outputs.tf

--- kafka/outputs.tf
+++ kafka/outputs.tf
@@ -1,3 +1,6 @@
+data "aws_msk_bootstrap_brokers" "kafka-servers" {
+  cluster_arn = aws_msk_serverless_cluster.hotel-kafka.arn
+}
 output "kafka-server" {
-  value = aws_msk_cluster.hotel-kafka.bootstrap_brokers_tls
+  value = data.aws_msk_bootstrap_brokers.kafka-servers.bootstrap_brokers_tls
 }
