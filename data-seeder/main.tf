288d8ddc8196f2c27f73ef0a5e1e0cdc1febb7ad
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 21:42:18 +0700

    Update main.tf

--- data-seeder/main.tf
+++ data-seeder/main.tf
@@ -1,16 +1,26 @@
+resource "aws_db_parameter_group" "source-db" {
+  name   = "${var.source-db-name}-postgres15.6"
+  family = "postgres15.6"
+  parameter {
+    name  = "logical_replication"
+    value = "1"
+  }
+}
+
 resource "aws_db_instance" "source-db" {
-  instance_class      = "db.t3.micro"
-  engine              = "postgres"
-  engine_version      = "15.6"
-  skip_final_snapshot = true
-  multi_az            = false
-  identifier          = var.source-db-name
-  db_name             = var.source-db-name
-  username            = var.source-db-username
-  password            = var.source-db-password
-  allocated_storage   = 5
-  publicly_accessible = true
-  availability_zone   = var.aws-zone
+  instance_class       = "db.t3.micro"
+  engine               = "postgres"
+  engine_version       = "15.6"
+  skip_final_snapshot  = true
+  multi_az             = false
+  identifier           = var.source-db-name
+  db_name              = var.source-db-name
+  username             = var.source-db-username
+  password             = var.source-db-password
+  allocated_storage    = 5
+  publicly_accessible  = true
+  availability_zone    = var.aws-zone
+  parameter_group_name = aws_db_parameter_group.source-db.name
 }
 
 resource "aws_instance" "data-seeder" {
