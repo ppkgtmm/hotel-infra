68529cebb802543bf7abddeca61fa998858911c3
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 14:16:51 +0700

    Update main.tf

--- connector/main.tf
+++ connector/main.tf
@@ -1,3 +1,9 @@
 resource "aws_instance" "connector" {
-
+  instance_type        = var.aws-instance-type
+  ami                  = var.aws-ami
+  iam_instance_profile = var.connector-role
+  tags = {
+    Name = "connector-setup"
+  }
+  availability_zone = var.aws-zone
 }
