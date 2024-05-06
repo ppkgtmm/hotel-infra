fae4a30fb306a27878d27b07b001ed653e54a223
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 15:40:38 +0700

    Update setup.sh

--- connector/setup.sh
+++ connector/setup.sh
@@ -1,8 +1,9 @@
 apt update
 apt install -y wget awscli zip tar
 mkdir plugin
+mkdir $PLUGIN
 wget -O $PLUGIN.tar.gz https://repo1.maven.org/maven2/io/debezium/debezium-connector-postgres/2.6.1.Final/$PLUGIN.Final-plugin.tar.gz
-tar xzf $PLUGIN.tar.gz
-sudo zip -r $PLUGIN.zip $PLUGIN
-sudo mv $PLUGIN.zip plugin/
+tar xzf $PLUGIN.tar.gz -C $PLUGIN
+zip -r $PLUGIN.zip $PLUGIN
+mv $PLUGIN.zip plugin/
 aws s3 cp plugin s3://$S3_BUCKET/plugin --recursive --region $AWS_REGION
