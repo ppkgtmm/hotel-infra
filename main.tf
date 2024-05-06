53ad52c99b53a53f30d7ea135cf448d3dd72a9f9
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-06 14:01:23 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,31 +19,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "google" {
-  project = var.gcp-project-id
-  region  = var.gcp_region
-  zone    = var.gcp_zone
-}
-
 provider "random" {}
 
 data "aws_caller_identity" "current" {}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_postgres" {
-  security_group_id = data.aws_security_group.default.id
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "${module.kafka-connect.kafka-connect-ip}/32"
-  from_port         = 5432
-  to_port           = 5432
-}
 
 module "data-generator" {
   source     = "./data-generator"
@@ -51,25 +29,17 @@ module "data-generator" {
   aws_region = var.aws_region
   account-id = data.aws_caller_identity.current.account_id
 }
-module "data-seeder" {
-  source             = "./data-seeder"
-  source-db-name     = var.source-db-name
-  source-db-password = var.source-db-password
-  source-db-username = var.source-db-username
-  aws_zone           = var.aws_zone
-  aws_region         = var.aws_region
-  bucket-id          = module.data-generator.bucket-id
-}
 
-module "kafka" {
-  source         = "./kafka"
-  gcp_network    = var.gcp_network
-  gcp_disk_image = var.gcp_disk_image
+module "connector" {
+  source    = "./connector"
+  bucket-id = module.data-generator.bucket-id
 }
-
-module "kafka-connect" {
-  source         = "./kafka-connect"
-  gcp_network    = var.gcp_network
-  gcp_disk_image = var.gcp_disk_image
-  kafka_server   = module.kafka.kafka-server
-}
+# module "data-seeder" {
+#   source             = "./data-seeder"
+#   source-db-name     = var.source-db-name
+#   source-db-password = var.source-db-password
+#   source-db-username = var.source-db-username
+#   aws_zone           = var.aws_zone
+#   aws_region         = var.aws_region
+#   bucket-id          = module.data-generator.bucket-id
+# }
