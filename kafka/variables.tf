ce92de6bd4b24141cca41bd171a7b69c38738016
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-07 12:30:14 +0700

    Update variables.tf

--- kafka/variables.tf
+++ kafka/variables.tf
@@ -2,6 +2,14 @@ variable "gcp-disk-image" {
   type = string
 }
 
+variable "gcp-disk-type" {
+  type = string
+}
+
+variable "gcp-machine-type" {
+  type = string
+}
+
 variable "gcp-network" {
   type = string
 }
