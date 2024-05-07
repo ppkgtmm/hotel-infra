4a9f4460e88f6f63a4c5cc7049db919aebc2b916
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-08 00:24:53 +0700

    Update main.tf

--- kafka/main.tf
+++ kafka/main.tf
@@ -1,14 +1,33 @@
-resource "aws_msk_serverless_cluster" "hotel-kafka" {
-  cluster_name = "hotel-kafka"
-  vpc_config {
-    subnet_ids         = slice(var.subnets, 0, 2)
-    security_group_ids = [var.security-group-id]
-  }
-  client_authentication {
-    sasl {
-      iam {
-        enabled = true
-      }
+resource "random_uuid" "rand" {}
+
+resource "google_compute_instance" "kafka" {
+  for_each       = var.kafka_bootstrap_servers
+  machine_type   = var.gcp_machine_type
+  name           = each.value
+  enable_display = false
+  boot_disk {
+    initialize_params {
+      image = var.gcp_disk_image
+      size  = 10
+      type  = var.gcp_disk_type
     }
   }
+  network_interface {
+    network = var.gcp_network
+    access_config {
+    }
+  }
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+  }
+  metadata_startup_script = <<EOF
+#!/bin/bash
+export KAFKA_CLUSTER_ID=${random_uuid.rand.id}
+export KAFKA_NODE_ID=${each.key}
+export KAFKA_VOTERS=${join(",", [for id, server in var.kafka_bootstrap_servers : format("%s@%s:9093", id, server)])}
+${file("./kafka/setup.sh")}
+EOF
 }
