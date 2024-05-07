96927ffd2bc93d3255ce06b280a7c474f5b6478c
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-08 00:33:31 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.47.0"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "5.27.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.1"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = var.gcp_region
+  zone    = var.gcp_zone
 }
 
 data "aws_caller_identity" "current" {}
