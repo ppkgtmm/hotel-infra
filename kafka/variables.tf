95c4204b649872b47073de07cfea6bdc9f37d05e
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-08 00:24:51 +0700

    Update variables.tf

--- kafka/variables.tf
+++ kafka/variables.tf
@@ -1,7 +1,23 @@
-variable "subnets" {
-  type = list(string)
+variable "gcp_disk_image" {
+  type = string
 }
 
-variable "security-group-id" {
+variable "gcp_disk_type" {
   type = string
+}
+
+variable "gcp_machine_type" {
+  type = string
+}
+
+variable "gcp_network" {
+  type = string
+}
+
+variable "kafka_bootstrap_servers" {
+  default = {
+    1 : "kafka1"
+    # , 2 : "kafka2" 
+  }
+  type = map(number, string)
 }
