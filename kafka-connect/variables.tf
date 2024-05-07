5607e08577abbeb3f8c6d33e49929af07a2b087a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-07 17:40:08 +0700

    revert kafka connect

--- kafka-connect/variables.tf
+++ kafka-connect/variables.tf
@@ -1,11 +1,45 @@
-variable "gcp-disk-image" {
+variable "bucket-id" {
   type = string
 }
 
-variable "gcp-network" {
+variable "plugin-path" {
   type = string
 }
 
-variable "kafka-bootstrap-servers" {
+variable "kafka-servers" {
   type = string
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "security-group-id" {
+  type = string
+}
+
+variable "connect-role" {
+  type = string
+}
+
+variable "source-db-host" {
+  type = string
+}
+
+variable "source-db-port" {
+  type = string
+}
+
+variable "source-db-name" {
+  type = string
+}
+
+variable "replication-user" {
+  type      = string
+  sensitive = true
+}
+
+variable "replication-password" {
+  type      = string
+  sensitive = true
 }
