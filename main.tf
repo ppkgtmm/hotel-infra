47cb4c7de32b29b4d38ced4ed943425c254fc7ae
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-07 12:38:05 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,13 +33,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 data "aws_security_group" "default" {
   vpc_id = data.aws_vpc.default.id
 }
