c030860bcb0c3b13c8d8e8f8828c080fb121561d
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-08 00:35:39 +0700

    Update main.tf

--- kafka-connect/main.tf
+++ kafka-connect/main.tf
@@ -1,70 +1,40 @@
-resource "aws_mskconnect_worker_configuration" "hotel-connect" {
-  name                    = "hotel-connect"
-  properties_file_content = file("./kafka-connect/connect.properties")
-}
-
-data "aws_s3_bucket" "bucket" {
-  bucket = var.s3-bucket-name
-}
-
-data "aws_iam_role" "connect-role" {
-  name = var.connect-role
-}
-
-resource "aws_mskconnect_custom_plugin" "plugin" {
-  name         = "debezium-postgres"
-  content_type = "ZIP"
-  location {
-    s3 {
-      bucket_arn = data.aws_s3_bucket.bucket.arn
-      file_key   = var.plugin-path
+resource "google_compute_instance" "kafka-connect" {
+  machine_type   = "e2-micro"
+  name           = "kafka-connect"
+  enable_display = false
+  tags           = ["kafka-connect"]
+  boot_disk {
+    initialize_params {
+      image = var.gcp_disk_image
+      size  = 10
+      type  = "pd-standard"
     }
   }
+  network_interface {
+    network = var.gcp_network
+    access_config {
+    }
+  }
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible         = true
+    provisioning_model  = "SPOT"
+  }
+  metadata_startup_script = <<EOF
+#!/bin/bash
+export KAFKA_SERVERS=${var.kafka_bootstrap_servers}
+${file("./kafka-connect/setup.sh")}
+EOF
 }
 
-resource "aws_mskconnect_connector" "hotel-connect" {
-  name                 = "hotel-connect"
-  kafkaconnect_version = "2.7.1"
-  capacity {
-    autoscaling {
-      min_worker_count = 1
-      max_worker_count = 2
-    }
-  }
-  kafka_cluster {
-    apache_kafka_cluster {
-      bootstrap_servers = var.kafka-servers
-      vpc {
-        security_groups = [var.security-group-id]
-        subnets         = var.subnets
-      }
-    }
-  }
-  connector_configuration = {
-    "connector.class"     = "io.debezium.connector.postgresql.PostgresConnector"
-    "plugin.name"         = "pgoutput"
-    "tasks.max"           = "1"
-    "database.hostname"   = var.source-db-host
-    "database.port"       = var.source-db-port
-    "database.user"       = var.replication-user
-    "database.password"   = var.replication-password
-    "database.dbname"     = var.source-db-name
-    "topic.prefix"        = var.source-db-name
-    "schema.include.list" = "public"
-    "snapshot.mode"       = "when_needed"
-  }
-  kafka_cluster_client_authentication {}
-  kafka_cluster_encryption_in_transit {}
-
-  plugin {
-    custom_plugin {
-      arn      = aws_mskconnect_custom_plugin.plugin.arn
-      revision = aws_mskconnect_custom_plugin.plugin.latest_revision
-    }
-  }
-  service_execution_role_arn = data.aws_iam_role.connect-role.arn
-  worker_configuration {
-    arn      = aws_mskconnect_worker_configuration.hotel-connect.arn
-    revision = aws_mskconnect_worker_configuration.hotel-connect.latest_revision
+resource "google_compute_firewall" "name" {
+  network     = var.gcp_network
+  name        = "allow-kafka-internal"
+  target_tags = ["kafka"]
+  source_tags = ["kafka-connect"]
+  allow {
+    protocol = "tcp"
+    ports    = ["9092"]
   }
 }
