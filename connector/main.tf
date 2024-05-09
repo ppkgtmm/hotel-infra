f8b9f2515a474c3e601d977446103b3387303c69
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-09 19:54:17 +0700

    Update main.tf

--- connector/main.tf
+++ connector/main.tf
@@ -15,11 +15,18 @@ resource "google_cloudfunctions2_function" "connector" {
     max_instance_count    = 1
     environment_variables = { DB_HOST : var.source_db_host, DBZ_USER : var.replication_user, DBZ_PASSWORD : var.replication_password, DB_NAME : var.source_db_name, KAFKA_CONNECT_SERVER : var.kafka_connect_server }
   }
+  event_trigger {
+    event_type   = "google.cloud.storage.object.v1.finalized"
+    retry_policy = "RETRY_POLICY_RETRY"
+    event_filters {
+      attribute = "bucket"
+      value     = var.gcp_bucket_name
+    }
+  }
 }
 
-resource "google_cloud_run_service_iam_member" "member" {
-  location = google_cloudfunctions2_function.connector.location
-  service  = google_cloudfunctions2_function.connector.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+resource "google_storage_bucket_object" "name" {
+  name    = "trigger"
+  content = "just to trigger cloud function"
+  bucket  = var.gcp_bucket_name
 }
