65ca4e3de71047f1b87e55bd2964f9c117c984cc
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-09 16:51:39 +0700

    Update main.tf

--- connector/main.tf
+++ connector/main.tf
@@ -16,3 +16,10 @@ resource "google_cloudfunctions2_function" "connector" {
     environment_variables = { DB_HOST : var.source_db_host, DBZ_USER : var.replication_user, DBZ_PASSWORD : var.replication_password, DB_NAME : var.source_db_name, KAFKA_CONNECT_SERVER : var.kafka_connect_server }
   }
 }
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.connector.location
+  service  = google_cloudfunctions2_function.connector.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
