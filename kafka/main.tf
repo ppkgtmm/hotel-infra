848c790f70b2ab23fb1683f2b10222bc92adb7b1
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-10 21:44:23 +0700

    Update main.tf

--- kafka/main.tf
+++ kafka/main.tf
@@ -2,7 +2,7 @@ resource "aws_msk_serverless_cluster" "kafka" {
   cluster_name = "hotel-kafka"
   vpc_config {
     subnet_ids         = slice(tolist(var.aws_subnet_ids), 0, 2)
-    security_group_ids = [var.aws_vpc_id]
+    security_group_ids = [var.aws_security_group]
   }
   client_authentication {
     sasl {
