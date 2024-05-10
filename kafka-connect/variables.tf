0860a4e97ec373a29386be3ae3697e16a4b9288b
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-10 19:25:45 +0700

    Update variables.tf

--- kafka-connect/variables.tf
+++ kafka-connect/variables.tf
@@ -13,35 +13,3 @@ variable "kafka_connect_role" {
 variable "kafka_bootstrap_servers" {
   type = string
 }
-
-variable "source_db_name" {
-  default = "hotel"
-}
-
-variable "source_db_username" {
-  type      = string
-  sensitive = true
-}
-
-variable "source_db_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "source_db_host" {
-  type = string
-}
-
-variable "source_db_port" {
-  type = string
-}
-
-variable "replication_user" {
-  type      = string
-  sensitive = true
-}
-
-variable "replication_password" {
-  type      = string
-  sensitive = true
-}
