bb95e1b85a8f0574bbf20ad46f057625427e8c49
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-11 18:58:43 +0700

    Update main.tf

--- kafka/main.tf
+++ kafka/main.tf
@@ -1,25 +1,18 @@
-resource "aws_msk_configuration" "config" {
-  name              = "hotel-kafka-config"
-  server_properties = file("./kafka/kafka.properties")
-}
-
-resource "aws_msk_cluster" "kafka" {
-  cluster_name           = "hotel-kafka"
-  kafka_version          = "3.5.1"
-  number_of_broker_nodes = 2
-
-  broker_node_group_info {
-    instance_type  = "kafka.t3.small"
-    client_subnets = slice(tolist(var.aws_subnet_ids), 0, 2)
-    storage_info {
-      ebs_storage_info {
-        volume_size = 10
+resource "aws_msk_serverless_cluster" "kafka" {
+  cluster_name = "hotel-kafka"
+  vpc_config {
+    subnet_ids         = slice(tolist(var.aws_subnet_ids), 0, 2)
+    security_group_ids = [var.aws_security_group]
+  }
+  client_authentication {
+    sasl {
+      iam {
+        enabled = true
       }
     }
-    security_groups = [var.aws_security_group]
   }
-  configuration_info {
-    arn      = aws_msk_configuration.config.arn
-    revision = aws_msk_configuration.config.latest_revision
-  }
+}
+
+data "aws_msk_bootstrap_brokers" "kafka_brokers" {
+  cluster_arn = aws_msk_serverless_cluster.kafka.arn
 }
