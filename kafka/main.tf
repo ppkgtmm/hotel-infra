25a5dd3f939dc4d1330f7c8b32e8d4337b6909d3
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-13 01:21:06 +0700

    Update main.tf

--- kafka/main.tf
+++ kafka/main.tf
@@ -1,16 +1,5 @@
-resource "aws_msk_cluster" "hotel_kafka" {
-  cluster_name           = "hotel-kafka"
-  kafka_version          = "3.2.0"
-  number_of_broker_nodes = 2
-
-  broker_node_group_info {
-    instance_type  = "kafka.t3.small"
-    client_subnets = slice(tolist(var.aws_subnets), 0, 2)
-    storage_info {
-      ebs_storage_info {
-        volume_size = 10
-      }
-    }
-    security_groups = [var.aws_security_group]
-  }
+resource "aws_network_interface" "kafka_network_interface" {
+  subnet_id         = var.aws_subnet_id
+  private_ips_count = 3
+  security_groups   = [var.aws_security_group]
 }
