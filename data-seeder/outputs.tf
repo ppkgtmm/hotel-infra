dedf6c73769aea64e8e94b243778a20f0cfc1ff7
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-13 20:21:42 +0700

    remove files

--- data-seeder/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "source_db_host" {
-  value = aws_db_instance.source_db.address
-}
-
-output "source_db_port" {
-  value = aws_db_instance.source_db.port
-}
--- data-seeder/setup.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install -y git python3 python3-venv wget libpq-dev postgresql-client gcc python3-dev
-git clone https://github.com/ppkgtmm/hotel-seed.git dataseed
-cd dataseed
-wget -O .env https://raw.githubusercontent.com/ppkgtmm/hotel-infra/main/.env
-./run.sh
