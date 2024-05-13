ae8e88305052c4cd21421012a966054d24fc2166
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-13 20:25:27 +0700

    combine files

--- sourcedb.tf
+++ sourcedb.tf
@@ -1,3 +1,25 @@
+locals {
+  data_generator_variables = {
+    GIT_REPO = "https://github.com/ppkgtmm/hotel-datagen.git"
+    ENVIRONMENT = {
+      LOCATION_FILE = "https://github.com/ppkgtmm/location/raw/main/states_provinces.csv"
+      SEED          = 42
+      SEED_DIR      = var.seed_directory
+    }
+  }
+}
+
+resource "aws_instance" "data_generator" {
+  instance_type        = var.instance_type
+  ami                  = var.ubuntu_ami
+  user_data            = templatefile("./initialize.sh", local.data_generator_variables)
+  iam_instance_profile = "s3-access"
+  tags = {
+    Name = "data-seeder"
+  }
+  availability_zone = var.availability_zone
+}
+
 resource "aws_db_parameter_group" "source_db" {
   name   = "${var.source_db_name}-postgres15-6"
   family = "postgres15"
@@ -46,4 +68,5 @@ resource "aws_instance" "data-seeder" {
     Name = "data-seeder"
   }
   availability_zone = var.availability_zone
+  depends_on        = [aws_db_instance.source_db, aws_instance.data_generator]
 }
