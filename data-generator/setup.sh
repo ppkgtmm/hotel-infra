3b56e346ec387d413573d33aed7d40a3063ef20c
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-13 17:30:45 +0700

    Delete setup.sh

--- data-generator/setup.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install -y git python3 python3-venv wget
-git clone https://github.com/ppkgtmm/hotel-datagen.git datagen
-cd datagen
-wget -O .env https://raw.githubusercontent.com/ppkgtmm/hotel-infra/main/.env
-./run.sh
