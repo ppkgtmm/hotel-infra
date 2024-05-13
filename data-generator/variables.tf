f701a5f22a11bb1291529d0e97c4912ce3ba6a10
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-13 16:59:48 +0700

    Update variables.tf

--- data-generator/variables.tf
+++ data-generator/variables.tf
@@ -2,22 +2,34 @@ variable "aws_region" {
   type = string
 }
 
-variable "aws_zone" {
-  type = string
-}
-
 variable "data_generator_role" {
   type = string
 }
 
-variable "aws_ami" {
-  type = string
-}
-
-variable "aws_instance_type" {
-  type = string
-}
-
 variable "s3_bucket_name" {
+  type = string
+}
+
+variable "function_zip_file" {
+  default = "hotel-data-generator.zip"
+}
+
+variable "aws_security_group" {
+  type = string
+}
+
+variable "aws_subnet_ids" {
+  type = list(string)
+}
+
+variable "location_file" {
+  type = string
+}
+
+variable "seed" {
+  type = number
+}
+
+variable "seed_dir" {
   type = string
 }
