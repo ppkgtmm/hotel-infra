345b70773cd69af1f30068ec5d72aaacc4afde59
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-14 00:03:37 +0700

    Update sourcedb.tf

--- sourcedb.tf
+++ sourcedb.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "data_generator" {
   instance_type        = var.instance_type
   ami                  = var.ubuntu_ami
   user_data            = templatefile("./initialize.sh", local.data_generator_variables)
-  iam_instance_profile = "s3-ec2-access"
+  iam_instance_profile = "s3-access"
   tags = {
     Name = "data-generator"
   }
