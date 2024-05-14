1da05f7331c7f8faf445a0a67d903b6a91a5b87a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-14 14:16:35 +0700

    Update initialize.sh

--- debezium/initialize.sh
+++ debezium/initialize.sh
@@ -4,20 +4,14 @@ wget -O debezium-server.tar.gz https://repo1.maven.org/maven2/io/debezium/debezi
 tar xzf debezium-server.tar.gz
 cd debezium-server && mkdir -p data/
 wget -O application.properties https://github.com/ppkgtmm/hotel-infra/raw/main/debezium/application.properties
-export KAFKA_SERVER=${KAFKA_SERVER}
-export DB_HOST=${DB_HOST}
-export DB_PORT=${DB_PORT}
-export DB_USER=${DB_USER}
-export DB_PASSWORD=${DB_PASSWORD}
-export DB_NAME=${DB_NAME}
 cat >> application.properties <<EOF
-debezium.sink.kafka.producer.bootstrap.servers=$KAFKA_SERVER
-debezium.source.database.hostname=$DB_HOST
-debezium.source.database.port=$DB_PORT
-debezium.source.database.user=$DB_USER
-debezium.source.database.password=$DB_PASSWORD
-debezium.source.database.dbname=$DB_NAME
-debezium.source.topic.prefix=$DB_NAME
+debezium.sink.kafka.producer.bootstrap.servers=${KAFKA_SERVER}
+debezium.source.database.hostname=${DB_HOST}
+debezium.source.database.port=${DB_PORT}
+debezium.source.database.user=${DB_USER}
+debezium.source.database.password=${DB_PASSWORD}
+debezium.source.database.dbname=${DB_NAME}
+debezium.source.topic.prefix=${DB_NAME}
 EOF
 mv application.properties conf/
 chmod +x run.sh && ./run.sh
