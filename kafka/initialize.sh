01d4bab298fcc26ee8ef13d491f1dab0c0fa1b0f
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-14 22:33:29 +0700

    Update initialize.sh

--- kafka/initialize.sh
+++ kafka/initialize.sh
@@ -6,6 +6,7 @@ wget -O config.properties https://github.com/ppkgtmm/hotel-infra/raw/main/kafka/
 cat >> config.properties <<EOF
 node.id=${NODE_ID}
 controller.quorum.voters=${VOTERS}
+advertised.listeners=PLAINTEXT://${PUBLIC_IP}:9092
 EOF
 _JAVA_OPTIONS="-Xms128m -Xmx512m -Djava.net.preferIPv4Stack=true" bin/kafka-storage.sh format -t ${KAFKA_CLUSTER_ID} -c config.properties
 _JAVA_OPTIONS="-Xms128m -Xmx512m -Djava.net.preferIPv4Stack=true" bin/kafka-server-start.sh config.properties 
