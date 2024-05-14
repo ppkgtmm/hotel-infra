1ab956f102a1a5bc462ab5e083e1118c2729b817
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-14 13:53:50 +0700

    Update initialize.sh

--- kafka/initialize.sh
+++ kafka/initialize.sh
@@ -2,9 +2,11 @@ apt update && apt install -y wget default-jdk
 wget -O kafka.tgz https://dlcdn.apache.org/kafka/3.7.0/kafka_2.13-3.7.0.tgz
 tar xzf kafka.tgz && cd kafka_2.13-3.7.0
 wget -O config.properties https://github.com/ppkgtmm/hotel-infra/raw/main/kafka/kafka.properties
+export NODE_ID=${NODE_ID}
+export VOTERS=${VOTERS}
 cat >> config.properties <<EOF
-node.id=${NODE_ID}
-controller.quorum.voters=${VOTERS}
+node.id=$NODE_ID
+controller.quorum.voters=$VOTERS
 EOF
 export _JAVA_OPTIONS="-Djava.net.preferIPv4Stack=true"
 bin/kafka-storage.sh format -t ${KAFKA_CLUSTER_ID} -c config.properties -Xmx256m
