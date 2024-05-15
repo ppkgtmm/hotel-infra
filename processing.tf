ed60fb5c44bb6d49aceaeabc8781d15adb42e06b
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 20:12:21 +0700

    Update processing.tf

--- processing.tf
+++ processing.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "hotel_processor" {
       DWH_NAME     = var.warehouse_db_name
     }
   }
-  depends_on = [aws_redshift_cluster.hotel_dwh]
+  # depends_on = [aws_redshift_cluster.hotel_dwh]
 }
 
 resource "aws_lambda_invocation" "processor_invocation" {
