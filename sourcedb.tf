e6897fdf1465b95275e3bf8bd5eb091fa9ace14b
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 20:06:10 +0700

    Update sourcedb.tf

--- sourcedb.tf
+++ sourcedb.tf
@@ -105,7 +105,7 @@ resource "aws_lambda_function" "hotel_connector" {
   depends_on = [aws_instance.data_seeder]
 }
 
-resource "aws_lambda_invocation" "invocation" {
+resource "aws_lambda_invocation" "connector_invocation" {
   function_name = aws_lambda_function.hotel_connector.function_name
   input         = jsonencode({})
   # depends_on    = [aws_lambda_function.hotel_connector]
