f6870607c324783688d3d892c241a0baa2272d84
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 18:17:11 +0700

    Create processing.tf

---
+++ processing.tf
@@ -0,0 +1,8 @@
+resource "aws_redshift_cluster" "hotel_dwh" {
+  node_type          = "dc2.large"
+  cluster_identifier = "data-warehouse"
+  cluster_type       = "single-node"
+  database_name      = var.warehouse_db_name
+  master_username    = var.warehouse_db_username
+  master_password    = var.warehouse_db_password
+}
