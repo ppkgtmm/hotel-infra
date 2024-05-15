e6fda4226acc14a9a4ebfc379db822650296fd1f
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 17:30:50 +0700

    Update streaming.tf

--- streaming.tf
+++ streaming.tf
@@ -1,38 +1,3 @@
-data "aws_iam_role" "connector_role" {
-  name = "s3-ec2-access"
-}
-
-resource "aws_lambda_function" "hotel_connector" {
-  function_name = "hotel_connector"
-  role          = data.aws_iam_role.connector_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
-  s3_bucket     = var.s3_bucket_name
-  s3_key        = "hotel-connector.zip"
-  skip_destroy  = false
-  timeout       = 10
-  vpc_config {
-    security_group_ids = [data.aws_security_group.default.id]
-    subnet_ids         = data.aws_subnets.subnets.ids
-  }
-  environment {
-    variables = {
-      DB_NAME      = var.source_db_name
-      DB_HOST      = aws_db_instance.source_db.address
-      DB_USER      = var.source_db_username
-      DB_PASSWORD  = var.source_db_password
-      DBZ_USER     = var.replication_user
-      DBZ_PASSWORD = var.replication_password
-    }
-  }
-  depends_on = [aws_instance.data_seeder]
-}
-
-resource "aws_lambda_invocation" "invocation" {
-  function_name = aws_lambda_function.hotel_connector.function_name
-  input         = jsonencode({})
-  depends_on    = [aws_lambda_function.hotel_connector]
-}
 
 resource "aws_network_interface" "kafka_network_interface" {
   subnet_id       = slice(data.aws_subnets.subnets.ids, 0, 1)[0]
@@ -83,4 +48,8 @@ resource "aws_instance" "debezium" {
   }
   user_data  = templatefile("./debezium/initialize.sh", local.debezium_server_variables)
   depends_on = [aws_lambda_invocation.invocation, aws_instance.kafka]
+}
+
+output "kafka_ips" {
+  value = join(",", formatlist("%s:9092", aws_instance.kafka[*].public_ip))
 }
