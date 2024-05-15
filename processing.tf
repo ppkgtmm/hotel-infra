fb2edcf5796539836af3478b76a833f0d929111e
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 20:07:24 +0700

    Update processing.tf

--- processing.tf
+++ processing.tf
@@ -11,3 +11,33 @@ resource "aws_redshift_cluster" "hotel_dwh" {
 output "redshift_endpoint" {
   value = aws_redshift_cluster.hotel_dwh.endpoint
 }
+
+resource "aws_lambda_function" "hotel_processor" {
+  function_name = "hotel-processor"
+  role          = data.aws_iam_role.lambda_role.arn
+  handler       = "entrypoint.handler"
+  runtime       = "python3.12"
+  s3_bucket     = var.s3_bucket_name
+  s3_key        = "hotel-processor.zip"
+  skip_destroy  = false
+  timeout       = 10
+  vpc_config {
+    security_group_ids = [data.aws_security_group.default.id]
+    subnet_ids         = data.aws_subnets.subnets.ids
+  }
+  environment {
+    variables = {
+      DWH_USER     = var.warehouse_db_username
+      DWH_PASSWORD = var.warehouse_db_password
+      DWH_HOST     = aws_redshift_cluster.hotel_dwh.endpoint
+      DWH_NAME     = var.warehouse_db_name
+    }
+  }
+  depends_on = [aws_redshift_cluster.hotel_dwh]
+}
+
+resource "aws_lambda_invocation" "processor_invocation" {
+  function_name = aws_lambda_function.hotel_processor.function_name
+  input         = jsonencode({})
+  # depends_on    = [aws_lambda_function.hotel_connector]
+}
