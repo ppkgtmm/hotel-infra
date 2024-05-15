36c9ff1eea87fd1498c405266ae9c3b43e7e2d1a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-15 19:39:05 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,46 +33,6 @@ data "aws_security_group" "default" {
   name   = "default"
 }
 
-data "aws_iam_role" "connector_role" {
-  name = "s3-ec2-access"
-}
-
-resource "aws_lambda_function" "hotel_connector" {
-  function_name = "hotel_connector"
-  role          = data.aws_iam_role.connector_role.arn
-  handler       = "entrypoint.handler"
-  runtime       = "python3.12"
-  s3_bucket     = var.s3_bucket_name
-  s3_key        = "hotel-connector.zip"
-  skip_destroy  = false
-  timeout       = 10
-  vpc_config {
-    security_group_ids = [data.aws_security_group.default.id]
-    subnet_ids         = data.aws_subnets.subnets.ids
-  }
-  environment {
-    variables = {
-      DB_NAME      = var.source_db_name
-      DB_HOST      = aws_db_instance.source_db.address
-      DB_USER      = var.source_db_username
-      DB_PASSWORD  = var.source_db_password
-      DBZ_USER     = var.replication_user
-      DBZ_PASSWORD = var.replication_password
-      DWH_USER     = var.warehouse_db_username
-      DWH_PASSWORD = var.warehouse_db_password
-      DWH_HOST     = aws_redshift_cluster.hotel_dwh.endpoint
-      DWH_NAME     = var.warehouse_db_name
-    }
-  }
-  depends_on = [aws_instance.data_seeder]
-}
-
-resource "aws_lambda_invocation" "invocation" {
-  function_name = aws_lambda_function.hotel_connector.function_name
-  input         = jsonencode({})
-  # depends_on    = [aws_lambda_function.hotel_connector]
-}
-
 # module "connector" {
 #   source               = "./connector"
 #   aws_security_group   = data.aws_security_group.default.id
