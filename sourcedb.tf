45dc05d926a80b0614d9ba555fdb50dd791cedf0
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-19 22:36:06 +0700

    Update sourcedb.tf

--- sourcedb.tf
+++ sourcedb.tf
@@ -78,35 +78,3 @@ resource "aws_instance" "data_seeder" {
 data "aws_iam_role" "lambda_role" {
   name = "s3-ec2-access"
 }
-
-resource "aws_lambda_function" "hotel_connector" {
-  function_name = "hotel-connector"
-  role          = data.aws_iam_role.lambda_role.arn
-  handler       = "entrypoint.handler"
-  runtime       = "python3.12"
-  s3_bucket     = var.s3_bucket_name
-  s3_key        = "hotel-connector.zip"
-  skip_destroy  = false
-  timeout       = 10
-  vpc_config {
-    security_group_ids = [data.aws_security_group.default.id]
-    subnet_ids         = data.aws_subnets.subnets.ids
-  }
-  environment {
-    variables = {
-      DB_NAME      = var.source_db_name
-      DB_HOST      = aws_db_instance.source_db.address
-      DB_USER      = var.source_db_username
-      DB_PASSWORD  = var.source_db_password
-      DBZ_USER     = var.replication_user
-      DBZ_PASSWORD = var.replication_password
-    }
-  }
-  depends_on = [aws_instance.data_seeder]
-}
-
-resource "aws_lambda_invocation" "connector_invocation" {
-  function_name = aws_lambda_function.hotel_connector.function_name
-  input         = jsonencode({})
-  # depends_on    = [aws_lambda_function.hotel_connector]
-}
