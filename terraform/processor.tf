9eae097dd57ae9f09a9eb5538d7bc35aa53bd069
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-21 15:19:45 +0700

    Update processor.tf

--- terraform/processor.tf
+++ terraform/processor.tf
@@ -5,6 +5,20 @@ resource "aws_subnet" "private_subnet" {
   cidr_block              = "172.31.48.0/20"
 }
 
+resource "aws_route_table" "private_route" {
+  vpc_id = data.aws_vpc.default.id
+
+  route {
+    cidr_block = aws_subnet.private_subnet.cidr_block
+    gateway_id = "local"
+  }
+}
+
+resource "aws_route_table_association" "private_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route.id
+}
+
 resource "aws_emrserverless_application" "hotel_stream" {
   name          = "hotel-stream"
   release_label = "emr-7.0.0"
