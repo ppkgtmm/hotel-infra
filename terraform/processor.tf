90ecc2ae6158569a9b33084cc09f6d26df6e4446
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-22 19:45:22 +0700

    Update processor.tf

--- terraform/processor.tf
+++ terraform/processor.tf
@@ -29,11 +29,12 @@ data "aws_route_table" "main_table" {
 }
 
 resource "aws_vpc_endpoint" "emr_endpoint" {
-  vpc_id             = data.aws_vpc.default.id
-  service_name       = "com.amazonaws.${var.aws_region}.emr-serverless"
-  subnet_ids         = [data.aws_subnet.default_subnet.id]
-  security_group_ids = [data.aws_security_group.default.id]
-  vpc_endpoint_type  = "Interface"
+  vpc_id              = data.aws_vpc.default.id
+  service_name        = "com.amazonaws.${var.aws_region}.emr-serverless"
+  subnet_ids          = [data.aws_subnet.default_subnet.id]
+  security_group_ids  = [data.aws_security_group.default.id]
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
 }
 
 resource "aws_route" "emr_route" {
