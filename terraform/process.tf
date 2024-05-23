aed71a477ce4da41ed47f6a915b1ee1cede9c7f1
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-23 13:58:08 +0700

    move dwh

--- terraform/process.tf
+++ terraform/process.tf
@@ -27,3 +27,13 @@ resource "aws_lambda_invocation" "processor_invocation" {
   input         = jsonencode({})
   depends_on    = [aws_lambda_function.hotel_processor]
 }
+
+resource "aws_redshift_cluster" "data_warehouse" {
+  node_type           = "dc2.large"
+  cluster_identifier  = "data-warehouse"
+  cluster_type        = "single-node"
+  database_name       = var.warehouse_db_name
+  master_username     = var.warehouse_db_username
+  master_password     = var.warehouse_db_password
+  skip_final_snapshot = true
+}
