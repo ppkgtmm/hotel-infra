7a7b3bfb80ded8b7c89d21b7c92b3f919a8b4f61
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-23 14:02:44 +0700

    Update process.tf

--- terraform/process.tf
+++ terraform/process.tf
@@ -1,3 +1,13 @@
+resource "aws_redshift_cluster" "data_warehouse" {
+  node_type           = "dc2.large"
+  cluster_identifier  = "data-warehouse"
+  cluster_type        = "single-node"
+  database_name       = var.warehouse_db_name
+  master_username     = var.warehouse_db_username
+  master_password     = var.warehouse_db_password
+  skip_final_snapshot = true
+}
+
 resource "aws_lambda_function" "hotel_processor" {
   function_name = "hotel-processor"
   role          = data.aws_iam_role.lambda_role.arn
@@ -26,14 +36,4 @@ resource "aws_lambda_invocation" "processor_invocation" {
   function_name = aws_lambda_function.hotel_processor.function_name
   input         = jsonencode({})
   depends_on    = [aws_lambda_function.hotel_processor]
-}
-
-resource "aws_redshift_cluster" "data_warehouse" {
-  node_type           = "dc2.large"
-  cluster_identifier  = "data-warehouse"
-  cluster_type        = "single-node"
-  database_name       = var.warehouse_db_name
-  master_username     = var.warehouse_db_username
-  master_password     = var.warehouse_db_password
-  skip_final_snapshot = true
 }
