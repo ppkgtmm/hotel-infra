a138cf4227e9802d0bf7b3e65cb0e7917a98621a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-29 20:42:11 +0700

    Update initialize.sh

--- data-streaming/debezium/initialize.sh
+++ data-streaming/debezium/initialize.sh
@@ -3,7 +3,7 @@ apt update && apt install -y wget default-jdk
 wget -O debezium-server.tar.gz https://repo1.maven.org/maven2/io/debezium/debezium-server-dist/2.6.1.Final/debezium-server-dist-2.6.1.Final.tar.gz
 tar xzf debezium-server.tar.gz
 cd debezium-server && mkdir -p data/
-wget -O application.properties https://github.com/ppkgtmm/hotel-infra/raw/main/debezium/application.properties
+wget -O application.properties https://github.com/ppkgtmm/hotel-infra/raw/main/data-streaming/debezium/application.properties
 cat >> application.properties <<EOF
 debezium.sink.kafka.producer.bootstrap.servers=${KAFKA_SERVER}
 debezium.source.database.hostname=${DB_HOST}
