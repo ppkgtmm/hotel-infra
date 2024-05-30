93d28088812a55aa06c0e5508b06fc62eb79ae25
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 00:36:53 +0700

    Update main.tf

--- data-generator/main.tf
+++ data-generator/main.tf
@@ -1,23 +1,25 @@
-locals {
-  data_generator_variables = {
-    GIT_REPO = "https://github.com/ppkgtmm/hotel-datagen.git"
-    ENVIRONMENT = {
-      LOCATION_FILE = "https://github.com/ppkgtmm/location/raw/main/states_provinces.csv"
-      SEED          = 42
-      SEED_DIR      = var.seed_directory
-      S3_BUCKET     = var.s3_bucket_name
-      AWS_REGION    = var.aws_region
+resource "google_cloudfunctions2_function" "datagen" {
+  location = var.google_cloud_region
+  name     = "hotel-datagen"
+  build_config {
+    runtime     = "python3.12"
+    entry_point = "get_generated_data"
+    source {
+      storage_source {
+        bucket = var.google_cloud_bucket
+        object = "hotel-datagen.zip"
+      }
     }
   }
-}
-
-resource "aws_instance" "data_generator" {
-  instance_type        = var.instance_type
-  ami                  = var.ubuntu_ami
-  user_data            = templatefile("initialize.sh", local.data_generator_variables)
-  iam_instance_profile = "s3-access"
-  tags = {
-    Name = "data-generator"
+  service_config {
+    min_instance_count = 1
+    max_instance_count = 1
+    timeout_seconds    = 300
+    environment_variables = {
+      LOCATION_FILE = var.location_file
+      SEED          = var.seed
+      SEED_DIR      = var.seed_directory
+      GCS_BUCKET    = var.google_cloud_bucket
+    }
   }
-  availability_zone = var.availability_zone
 }
