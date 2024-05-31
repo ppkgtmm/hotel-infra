10f7fd7e1d38c07f311f11ea82d5e13829a26020
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 12:22:46 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,70 +1,17 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.47.0"
-    }
-    random = {
-      source  = "hashicorp/random"
       version = "3.6.1"
     }
   }
 }
 
-provider "aws" { region = var.aws_region }
-
-provider "random" {}
-
-data "aws_vpc" "default" { default = true }
-
-data "aws_subnet" "default_subnet" {
-  availability_zone = var.availability_zone
-  default_for_az    = true
-}
-
-data "aws_subnets" "subnets" {}
-
-data "aws_security_group" "default" {
-  vpc_id = data.aws_vpc.default.id
-  name   = "default"
-}
-
-resource "aws_msk_configuration" "hotel_config" {
-  kafka_versions    = ["3.5.1"]
-  name              = "hotel-config"
-  server_properties = "auto.create.topics.enable=true"
-}
-
-resource "aws_msk_cluster" "hotel_kafka" {
-  number_of_broker_nodes = 2
-  broker_node_group_info {
-    instance_type   = "kafka.t3.small"
-    security_groups = [data.aws_security_group.default.id]
-    client_subnets  = slice(data.aws_subnets.subnets.ids, 0, 2)
-    storage_info {
-      ebs_storage_info {
-        volume_size = 5
-      }
-    }
-  }
-  cluster_name  = "hotel-kafka"
-  kafka_version = "3.5.1"
-  configuration_info {
-    arn      = aws_msk_configuration.hotel_config.arn
-    revision = aws_msk_configuration.hotel_config.latest_revision
-  }
+provider "google" {
+  project = var.google_cloud_project
+  region  = var.google_cloud_region
+  zone    = var.google_cloud_zone
 }
 
 module "data_generator" {
-  source            = "./data-generator"
-  aws_region        = var.aws_region
-  ubuntu_ami        = var.ubuntu_ami
-  instance_type     = var.instance_type
-  seed_directory    = var.seed_directory
-  s3_bucket_name    = var.s3_bucket_name
-  availability_zone = var.availability_zone
-  depends_on        = [aws_msk_cluster.hotel_kafka]
-}
 
 module "data_seeder" {
   source             = "./data-seeder"
@@ -94,4 +41,12 @@ module "data_streaming" {
   security_groups      = [data.aws_security_group.default.id]
   bootstrap_brokers    = aws_msk_cluster.hotel_kafka.bootstrap_brokers_tls
   depends_on           = [module.data_seeder]
+}
+  source                    = "./data-generator"
+  location_file             = var.location_file
+  seed                      = var.seed
+  seed_directory            = var.seed_directory
+  google_cloud_region       = var.google_cloud_region
+  google_cloud_bucket       = var.google_cloud_bucket
+  terraform_service_account = var.terraform_service_account
 }
