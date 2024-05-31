f301e174ff06a151ea95bd2e6b6891db03b43b75
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 18:34:02 +0700

    Update initialize.sh

--- data-streaming/debezium/initialize.sh
+++ data-streaming/debezium/initialize.sh
@@ -5,6 +5,7 @@ tar xzf debezium-server.tar.gz
 cd debezium-server && mkdir -p data/
 wget -O application.properties https://github.com/ppkgtmm/hotel-infra/raw/main/data-streaming/debezium/application.properties
 cat >> application.properties <<EOF
+debezium.sink.pubsub.project.id=${GCP_PROJECT_ID}
 debezium.source.database.hostname=${DB_HOST}
 debezium.source.database.user=${DB_USER}
 debezium.source.database.password=${DB_PASSWORD}
