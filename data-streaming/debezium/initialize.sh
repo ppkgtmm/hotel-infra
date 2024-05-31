b9ee635a5f53de25d0f794808e6e291a00ababad
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 19:26:25 +0700

    Update initialize.sh

--- data-streaming/debezium/initialize.sh
+++ data-streaming/debezium/initialize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt install -y wget default-jdk
+apt update && apt install -y wget default-jdk postgresql-client
 wget -O debezium-server.tar.gz https://repo1.maven.org/maven2/io/debezium/debezium-server-dist/2.6.1.Final/debezium-server-dist-2.6.1.Final.tar.gz
 tar xzf debezium-server.tar.gz
 cd debezium-server && mkdir -p data/
@@ -13,4 +13,5 @@ debezium.source.database.dbname=${DB_NAME}
 debezium.source.topic.prefix=${DB_NAME}
 EOF
 mv application.properties conf/
+psql postgresql://root:$ROOT_PASSWORD@$DB_HOST/$DB_NAME -c ""
 chmod +x run.sh && ./run.sh
