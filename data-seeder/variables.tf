5e96260456d8ebceef21cebeeb04aced174aac2a
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 17:09:44 +0700

    Update variables.tf

--- data-seeder/variables.tf
+++ data-seeder/variables.tf
@@ -2,6 +2,26 @@ variable "google_cloud_region" {
   type = string
 }
 
+variable "google_cloud_bucket" {
+  type = string
+}
+
+variable "terraform_service_account" {
+  type = string
+}
+
+variable "seed_directory" {
+  type = string
+}
+
+variable "location_file" {
+  type = string
+}
+
+variable "seed" {
+  type = number
+}
+
 variable "source_db_name" {
   default = "hotel"
 }
