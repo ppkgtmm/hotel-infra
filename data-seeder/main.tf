0be0f65d9bf4f6bb0ca78552b6ce87695d7a17c1
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 17:10:22 +0700

    Update main.tf

--- data-seeder/main.tf
+++ data-seeder/main.tf
@@ -35,3 +35,48 @@ resource "google_sql_user" "hotel_user" {
   name     = var.source_db_username
   password = var.source_db_password
 }
+
+resource "google_cloudfunctions2_function" "dataseed" {
+  location = var.google_cloud_region
+  name     = "hotel-dataseed"
+  build_config {
+    runtime     = "python312"
+    entry_point = "populate_source_db"
+    source {
+      storage_source {
+        bucket = var.google_cloud_bucket
+        object = "hotel-dataseed.zip"
+      }
+    }
+  }
+  service_config {
+    min_instance_count = 1
+    max_instance_count = 1
+    timeout_seconds    = 300
+    environment_variables = {
+      LOCATION_FILE = var.location_file
+      SEED          = var.seed
+      SEED_DIR      = var.seed_directory
+      GCS_BUCKET    = var.google_cloud_bucket
+      CONNECTION    = google_sql_database_instance.hotel_instance.connection_name
+      DB_USER       = var.source_db_username
+      DB_PASSWORD   = var.source_db_password
+      DB_NAME       = var.source_db_name
+    }
+  }
+}
+
+data "google_service_account_id_token" "id_token" {
+  target_audience        = google_cloudfunctions2_function.dataseed.url
+  target_service_account = var.terraform_service_account
+}
+
+resource "null_resource" "trigger_datagen" {
+  provisioner "local-exec" {
+    command = "curl -X POST $URL -H \"Authorization: bearer $TOKEN\" -H \"Content-Type: application/json\""
+    environment = {
+      TOKEN = data.google_service_account_id_token.id_token.id_token
+      URL   = google_cloudfunctions2_function.dataseed.url
+    }
+  }
+}
