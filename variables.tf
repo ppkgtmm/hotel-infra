dec934ba1fb40987fdd1483b4b131718484c2469
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 19:29:35 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -34,11 +34,6 @@ variable "source_db_name" {
   default = "hotel"
 }
 
-variable "source_db_username" {
-  type      = string
-  sensitive = true
-}
-
 variable "source_db_password" {
   type      = string
   sensitive = true
