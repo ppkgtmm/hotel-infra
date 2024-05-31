04bb16894f5e7c3e342ea74d2c92c32a3c1a4876
Pinky Gautam <pkgtm1998@gmail.com>
2024-05-31 21:21:03 +0700

    Update outputs.tf

--- data-seeder/outputs.tf
+++ data-seeder/outputs.tf
@@ -1,3 +1,7 @@
 output "source_db_host" {
   value = google_sql_database_instance.hotel_instance.public_ip_address
 }
+
+output "source_db_connection" {
+  value = google_sql_database_instance.hotel_instance.connection_name
+}
