c81922b1dd3ad5d0d7efb8ea43f653fd5dc80231
Pinky Gautam <pkgtm1998@gmail.com>
2024-06-02 22:59:00 +0700

    Create variables.tf

---
+++ data-processor/variables.tf
@@ -0,0 +1,19 @@
+variable "google_cloud_project" {
+  type = string
+}
+
+variable "google_cloud_region" {
+  type = string
+}
+
+variable "google_cloud_zone" {
+  type = string
+}
+
+variable "google_cloud_bucket" {
+  type = string
+}
+
+variable "terraform_service_account" {
+  type = string
+}
